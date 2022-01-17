d135c12d1affcf9ff42ad70781f1198c6e716c08
NahianShabab <nsnbd134@gmail.com>
2022-01-17 15:25:51 +0600

    added the feature to add product in  request body

--- adminDashboard/product/productController.js
+++ adminDashboard/product/productController.js
@@ -4,16 +4,16 @@ const queryUndefinedHandler=require('../../middleware/queryUndefinedHandler');
 
 async function addProduct(req,res,next){
     try{
-        var title=queryUndefinedHandler.returnNullIfUndefined(req.query.title);
-        var price=queryUndefinedHandler.returnNullIfUndefined(req.query.price);
-        var summary=queryUndefinedHandler.returnNullIfUndefined(req.query.summary);
-        var isFeatured=queryUndefinedHandler.returnNullIfUndefined(req.query.isFeatured);
-        var isContinued=queryUndefinedHandler.returnNullIfUndefined(req.query.isContinued);
-        var stock=queryUndefinedHandler.returnNullIfUndefined(req.query.stock);
-        var discount=queryUndefinedHandler.returnNullIfUndefined(req.query.discount);
-        var discountExpireDate=queryUndefinedHandler.returnNullIfUndefined(req.query.discountExpireDate);
-        var category=queryUndefinedHandler.returnNullIfUndefined(req.query.category);
-        var manufacturer=queryUndefinedHandler.returnNullIfUndefined(req.query.manufacturer);
+        var title=queryUndefinedHandler.returnNullIfUndefined(req.body.title);
+        var price=queryUndefinedHandler.returnNullIfUndefined(req.body.price);
+        var summary=queryUndefinedHandler.returnNullIfUndefined(req.body.summary);
+        var isFeatured=queryUndefinedHandler.returnNullIfUndefined(req.body.isFeatured);
+        var isContinued=queryUndefinedHandler.returnNullIfUndefined(req.body.isContinued);
+        var stock=queryUndefinedHandler.returnNullIfUndefined(req.body.stock);
+        var discount=queryUndefinedHandler.returnNullIfUndefined(req.body.discount);
+        var discountExpireDate=queryUndefinedHandler.returnNullIfUndefined(req.body.discountExpireDate);
+        var category=queryUndefinedHandler.returnNullIfUndefined(req.body.category);
+        var manufacturer=queryUndefinedHandler.returnNullIfUndefined(req.body.manufacturer);
         var updatedByUserName=req.username;
         var result= productDBAPI.addProductDBAPI(title,price,summary,isFeatured,isContinued,updatedByUserName
             ,stock,discount,discountExpireDate,category,manufacturer);
